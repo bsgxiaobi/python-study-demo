40d9880179d969c2cc023279f88dfe7889521a18
bsgxiaobi <412238561@qq.com>
2023-07-09 18:02:48 +0800

    update: 不可变的元组-tuple

--- 1基础/2list和tuple.py
+++ 1基础/2list和tuple.py
@@ -3,6 +3,7 @@
 # list是一种有序的集合，可以随时添加和删除其中的元素
 classname = ['Michael', 'Bob', 'Tracy']
 print(f'数组：{classname}，长度：{len(classname)}')
+# 取最后一个元素，除了计算索引位置外，还可以用-1做索引，直接获取最后一个元素,以此类推，可以获取倒数第2个、倒数第3个
 print(f'第一个元素：{classname[0]}，最后一个元素：{classname[-1]}')
 
 # 添加元素
@@ -27,4 +28,36 @@ print(f'元素替换后：{classname}')
 L = ['Apple', 123, True]
 print(f'list里面的元素的数据类型也可以不同：{L}')
 
+# tuple，tuple和list非常类似，但是tuple一旦初始化就不能修改
+# tuple不可变，所以代码更安全。如果可能，能用tuple代替list就尽量用tuple
+# tuple的陷阱：当你定义一个tuple时，在定义的时候，tuple的元素就必须被确定下来
+# 如果要定义一个空的tuple，可以写成()
+# 但是，要定义一个只有1个元素的tuple，如果你这么定义：t = (1)，定义的不是tuple，是1这个数！
+tupleTest = ('Michael', 'Bob', 'Tracy')
+print(f'tupleTest:{tupleTest}')
+tupleOne = ('single') # 定义一个只有1个元素的tuple，必须加一个逗号,，来消除歧义
+print(f'tupleOne:{tupleOne}')
 
+
+# “可变的”tuple
+# tuple所谓的“不变”是说，tuple的每个元素，指向永远不变，tuple指向的list对象不变，但list本身是可变的
+t = ('a', 'b', ['A', 'B'])
+print(f'改变tuple前的值:{t}')
+t[2][0] = 'A1'
+t[2][1] = 'B1'
+print(f'改变tuple后的值:{t}')
+
+
+# 练习
+# 请用索引取出下面list的指定元素：
+L = [ 
+        ['Apple', 'Google', 'Microsoft'],
+        ['Java', 'Python', 'Ruby', 'PHP'],
+        ['Adam', 'Bart', 'Lisa'] 
+    ]
+# 打印Apple:
+print(f'练习,打印Apple:{L[0][0]}')
+# 打印Python:
+print(f'练习,打印Python:{L[1][1]}')
+# 打印Lisa:
+print(f'练习,打印Lisa:{L[2][2]}')
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # python-study-demo
 This python study demo
+教程源于廖雪峰网站的python教程：https://www.liaoxuefeng.com/wiki/1016959663602400
